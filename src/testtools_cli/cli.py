08951a80ad0773fc9ce65a36973e4a9f4cd93a2b
asiazhang <asiazhang2002@gmail.com>
2024-07-01 15:32:24 +0800

    Add init command (#1)
    * 增加初始化的参数提示
    
    * 增加python的脚手架代码
    
    * 继续完善python脚手架
    
    * 使用rich输出
    
    * 增加checker

--- src/testtools_cli/cli.py
+++ src/testtools_cli/cli.py
@@ -1,21 +1,41 @@
+import logging
+import sys
+from pathlib import Path
+from typing import Optional
+
 import typer
+from loguru import logger
+from rich.logging import RichHandler
 from rich.traceback import install
 from typing_extensions import Annotated
 
+from testtools_cli.generator.scaffold_checker import ScaffoldChecker
+from .generator.scaffold_generator import ScaffoldGenerator, LangType
+
 install(show_locals=True)
 
+FORMAT = "%(message)s"
+logging.basicConfig(
+    level="INFO", format=FORMAT, datefmt="[%X]", handlers=[RichHandler()]
+)
+
 app = typer.Typer(rich_markup_mode="markdown")
+
+log = logging.getLogger("rich")
 
 
 @app.command()
 def init(
-    lang: Annotated[
-        str,
-        typer.Argument(help="TestTool language you want to use"),
-    ],
+        workdir: Annotated[
+            Optional[str],
+            typer.Option(
+                help="Where you want the scaffolding code to be stored, defaulting to the current directory"
+            ),
+        ] = None,
+        verbose: Annotated[Optional[bool], typer.Option(help="Verbose output")] = False,
 ) -> None:
     """
-    **Init** a testsolar testtool
+    **Init** a testsolar testtool with guide
 
     Current supported languages:
 
@@ -27,11 +47,31 @@ def init(
 
     - java
     """
-    print("init a testsolar testtool")
+    if not verbose:
+        log.setLevel(logging.INFO)
+        logger.remove()
+        logger.add(sys.stdout, level="INFO")
+
+    tool_name = typer.prompt("Name of the test tool?")
+    pre_langs = "/".join([e.value for e in LangType])
+    lang = LangType(
+        typer.prompt(f"The language you want to use for development({pre_langs})?")
+    )
+
+    gen = ScaffoldGenerator(lang=lang, testtool_name=tool_name, workdir=workdir)
+    gen.generate()
 
 
 @app.command()
-def check() -> None:
+def check(
+        workdir: Annotated[
+            Optional[str],
+            typer.Option(
+                help="The test tool dir to check, defaulting to the current directory"
+            ),
+        ] = None,
+        verbose: Annotated[Optional[bool], typer.Option(help="Verbose output")] = False,
+) -> None:
     """
     **Check** if the testing tools are effective
 
@@ -39,6 +79,14 @@ def check() -> None:
 
     - Check the validity of the testing tool scripts
     """
+    if not verbose:
+        log.setLevel(logging.INFO)
+        logger.remove()
+        logger.add(sys.stdout, level="INFO")
+
+    working_dir = Path(workdir) if workdir else Path.cwd()
+    checker = ScaffoldChecker(working_dir)
+    checker.check_test_tool()
 
 
 def cli_entry() -> None:
